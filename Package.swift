409640d0fb746eceb9601ff401e1d8a5891490ae
genexusbot <builder@genexus.com>
2023-05-15 14:55:14 -0300

    Package version: 1.0.0-beta+20230515174624

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCImageGallery",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-1.0.0-beta+20230515153434.xcframework.zip",
-			checksum: "24bba0646f0e5acd6ac06b6c2638ed1b397c380fe73c37eda83d0264ba020955"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-1.0.0-beta+20230515174624.xcframework.zip",
+			checksum: "d4c060e8d9f13b8b999c2c335bfa3b1c7641d7149f0e4a611f6ff8795a7fa68f"
 		)
 	]
 )
