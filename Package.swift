58e13b280af6f00b688e322a08cb9adbca8af6d9
Build Er <builder@macmini6.local>
2023-05-16 14:57:14 -0300

    Package version: 1.0.0-beta+20230516174834

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCImageGallery",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-1.0.0-beta+20230515174624.xcframework.zip",
-			checksum: "d4c060e8d9f13b8b999c2c335bfa3b1c7641d7149f0e4a611f6ff8795a7fa68f"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-1.0.0-beta+20230516174834.xcframework.zip",
+			checksum: "5d1563943819772ddeae4b061734ed13743ecfda00b9cad2a5271d5ebbdb200b"
 		)
 	]
 )
