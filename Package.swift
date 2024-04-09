33dbdb3a915a63f5dccf6eb6786e26316ca3e890
Build Er <builder@macmini6.local>
2024-04-09 16:41:47 -0300

    Package version: 1.5.0-beta.29

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUCImageGalleryWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-beta.28")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-beta.29")
 	],
 	targets: [
 		.target(name: "GXUCImageGalleryWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCImageGallery",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-1.5.0-beta.28.xcframework.zip",
-			checksum: "ce105ad784106ccf6601b9340a86d973cd757edc2589bf0938df7b02b2358a20"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-1.5.0-beta.29.xcframework.zip",
+			checksum: "152278b16f636e42cd03daaa6c00ffbb062639fa0ac9e82a56abdafae502a819"
 		)
 	]
 )
