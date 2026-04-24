c7336d1a020037eadec9379be1c070271e2aeecb
The iOS Team @ GeneXus <builder@genexus.com>
2026-04-24 17:25:27 -0400

    Package version: 4.2.0-beta.7

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUCImageGalleryWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-beta.6")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-beta.7")
 	],
 	targets: [
 		.target(name: "GXUCImageGalleryWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCImageGallery",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-4.2.0-beta.6.xcframework.zip",
-			checksum: "adabd971c9053255498b31b28621c428e912e1c4772114da3f245f06dd235e44"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCImageGallery-4.2.0-beta.7.xcframework.zip",
+			checksum: "fff3ab276bb39074b8ba6b322a30952721478c6d903391f3b097cbada0505a99"
 		)
 	]
 )
